6d6bf952cd695d7d29b1bea3c065f5198ba25038
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-12 13:36:29 -0300

    Use @Component in DateUtil class

--- src/main/java/br/com/zup/springboot2/util/DateUtil.java
+++ src/main/java/br/com/zup/springboot2/util/DateUtil.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
+@Component
 public class DateUtil {
 
     public String formatlocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
